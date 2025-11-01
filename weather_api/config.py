c4069a9ab249be302e685b0cb6d421a9dc3abed3
Ayisha <ca20ayisharukshana.m@gmail.com>
2025-11-01 10:32:52 +0530

    weather_api add

--- weather_api/config.py
+++ weather_api/config.py
@@ -3,7 +3,7 @@ import os
 
 class Settings(BaseSettings):
     OPENWEATHER_API_KEY: str
-    JWT_SECRET: str
+    
 
     class Config:
         env_file = os.path.join(os.path.dirname(__file__), ".env")
